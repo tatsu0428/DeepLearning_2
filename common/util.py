5115b464fd98e2cd4baeec1907da7c2d0c404424
tatsu0428 <180441115@ccmailg.meijo-u.ac.jp>
2022-08-05 11:30:33 +0900

    Update util.py

--- common/util.py
+++ common/util.py
@@ -125,3 +125,30 @@ def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
         count += 1
         if count >= top:
             return
+
+
+def ppmi(C, verbose=False, eps=1e-8):
+    '''
+    PPMI（正の相互情報量）の作成
+
+    :param C: 共起行列
+    :param verbose: 進行状況を出力するかどうか
+    :return: PPMI行列
+    '''
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[i]*S[j]) + eps)
+            M[i, j] = max(0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total//100 + 1) == 0:
+                    print("%.1f%% done." % (100*cnt/total))
+
+    return M
